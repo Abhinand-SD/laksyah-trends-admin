3a6811933faa964fd8dbb2b3677cf4cd8b6fbd42
Abhinand SD <abhinandssd90@gmail.com>
2025-11-21 15:54:39 +0530

    add login page and slidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { assets } from '../assets/assets'
 
 const Sidebar = () => {
   return (
-    <div>
-        <div>
-            <NavLink to="/add">
-                <img src={assets.add_icon} alt="" />
-                <p>Add Items</p>
-            </NavLink>
+    <div className='w-[18%] min-h-screen border-r-1'>
+      <div className='flex flex-col gap-4 pt-6 pl-[20%] text-[15px]'>
 
-        </div>
+        <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-2 rounded-1' to="/add">
+          <img className='w-5 h-5' src={assets.add_icon} alt="" />
+          <p className='hidden md:block' >Add Items</p>
+        </NavLink>
+
+        <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-2 rounded-1' to="/list">
+          <img className='w-5 h-5' src={assets.order_icon} alt="" />
+          <p className='hidden md:block' >Add Items</p>
+        </NavLink>
+
+        <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-2 rounded-1' to="/orders">
+          <img className='w-5 h-5' src={assets.order_icon} alt="" />
+          <p className='hidden md:block' >Add Items</p>
+        </NavLink>
+
+
+      </div>
     </div>
   )
 }

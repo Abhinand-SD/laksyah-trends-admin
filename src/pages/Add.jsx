6d3c9ffbcd7e9d0541c32c92a669fcd906d2c0c9
Abhinand-SD <abhinandsd49@gmail.com>
2025-11-22 10:02:18 +0530

    add page start"

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Add = () => {
   return (
+   <form>
     <div>
-      
+      <p>Upload Image</p>
+
+      <div>
+        <label htmlFor=''>
+          <img src={assets.upload_area} alt="" />
+          <input type='file' name='' id='image1' hidden/>
+        </label>
+      </div>
     </div>
+   </form>
   )
 }
 

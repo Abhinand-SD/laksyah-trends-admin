ff118c0fbd22d37f4c02b9bbbc489c9dfe434ba9
Abhinand SD <abhinandssd90@gmail.com>
2025-11-30 04:03:27 +0530

    update to correct sidebar navlink name

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,19 +9,18 @@ const Sidebar = () => {
 
         <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-2 rounded-1' to="/add">
           <img className='w-5 h-5' src={assets.add_icon} alt="" />
-          <p className='hidden md:block' >Add Items</p>
+          <p className='hidden md:block'>Add Items</p>
         </NavLink>
 
         <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-2 rounded-1' to="/list">
           <img className='w-5 h-5' src={assets.order_icon} alt="" />
-          <p className='hidden md:block' >Add Items</p>
+          <p className='hidden md:block'>List Items</p>
         </NavLink>
 
         <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-2 rounded-1' to="/orders">
           <img className='w-5 h-5' src={assets.order_icon} alt="" />
-          <p className='hidden md:block' >Add Items</p>
+          <p className='hidden md:block'>Orders</p>
         </NavLink>
-
 
       </div>
     </div>
